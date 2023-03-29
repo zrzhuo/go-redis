0c03bcbc7662b00215fca0c49069377d3289e91f
zrzhuo <zrzhuo0226@163.com>
2023-03-29 23:10:10 +0800

    msg

--- main.go
+++ main.go
@@ -8,7 +8,7 @@ import (
 )
 
 func main() {
-	logger.Info("go-redis is running...")
+	logger.Info("go-redis is running.....")
 	redis.InitConfig("redis.conf") // 从redis.conf中读取配置
 	address := fmt.Sprintf("%s:%d", redis.Config.Bind, redis.Config.Port)
 	handler := tcp.MakeHandler()
